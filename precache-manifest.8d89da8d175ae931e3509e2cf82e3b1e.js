f7c92959cab3247a11f4a9506590ab34402bf6bc
Lauchlan Chisholm <lauchlan.chisholm@redspace.com>
2019-12-01 21:22:12 -0400

    Updates

---
+++ precache-manifest.8d89da8d175ae931e3509e2cf82e3b1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea5358e5b1e7093bd0e33c81921f2f8",
+    "url": "/potatoe-app/index.html"
+  },
+  {
+    "revision": "912cca7ffe7554fb8f05",
+    "url": "/potatoe-app/static/css/main.7549862c.chunk.css"
+  },
+  {
+    "revision": "97da8c493c22d09e9871",
+    "url": "/potatoe-app/static/js/2.2cb433bc.chunk.js"
+  },
+  {
+    "revision": "912cca7ffe7554fb8f05",
+    "url": "/potatoe-app/static/js/main.8ecf429a.chunk.js"
+  },
+  {
+    "revision": "a2ff75b09c91c8a7467a",
+    "url": "/potatoe-app/static/js/runtime-main.0083002b.js"
+  },
+  {
+    "revision": "ecc88a681ed3d3f8f220f0e3209add7d",
+    "url": "/potatoe-app/static/media/dancing-tato.ecc88a68.gif"
+  }
+]);
