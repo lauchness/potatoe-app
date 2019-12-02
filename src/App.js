1d910e29f8e20bc8684fba6bc913cc2c598e6122
Lauchlan Chisholm <lauchlan.chisholm@redspace.com>
2019-12-01 21:20:55 -0400

    Built entire app in one glorious potatoe sitting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import { useSpring, animated as a } from "react-spring";
+import translations from "./translations";
+import dancingTato from "./dancing-tato.gif";
+import "./App.css";
+
+const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
+const translate = (x, y) => `translate3d(${x / 2}px,0,0)`;
 
 function App() {
+  const indexRef = useRef(0);
+  const languages = Object.keys(translations);
+
+  console.log(languages);
+
+  const [currentLanguage, setCurrentLangauge] = useState(languages[0]);
+
+  const handleClickEvent = e => {
+    if (indexRef.current === languages.length - 1) {
+      indexRef.current = 0;
+    } else {
+      indexRef.current = indexRef.current + 1;
+    }
+    setCurrentLangauge(languages[indexRef.current]);
+  };
+
+  const [mouseSpring, setMouseSpring] = useSpring(() => ({
+    xy: [0, 0],
+    config: { mass: 10, tension: 550, friction: 40 }
+  }));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      onMouseMove={({ clientX: x, clientY: y }) =>
+        setMouseSpring({ xy: calc(x, y) })
+      }
+    >
+      <div className="sentence">{translations[currentLanguage].sentence}</div>
+      <button
+        className="change-language"
+        type="button"
+        onClick={handleClickEvent}
+      >
+        {translations[currentLanguage].button}
+      </button>
+      <a.div
+        className="potatoe"
+        style={{
+          transform: mouseSpring.xy.interpolate(translate)
+        }}
+      >
+        <img src={dancingTato} alt="Dancing potatoe" />
+      </a.div>
     </div>
   );
 }
---
+++ src/translations.js
@@ -0,0 +1,26 @@
+export default {
+  en: {
+    sentence: "Long live the potatoe",
+    button: "Change Language"
+  },
+  fr: {
+    sentence: "vive la pomme de terre",
+    button: "changer de langue"
+  },
+  de: {
+    sentence: "Es lebe die Kartoffel",
+    button: "Sprache ändern"
+  },
+  jp: {
+    sentence: "じゃがいもの長生き (Jagaimo no nagaiki)",
+    button: "言語を変えてください Gengo o kaete kudasai"
+  },
+  nl: {
+    sentence: "lang leve de aardappel",
+    button: "taal wijzigen"
+  },
+  pigLatin: {
+    sentence: "ongla ivela heta otatoepa",
+    button: "angecha anguagela"
+  }
+};
